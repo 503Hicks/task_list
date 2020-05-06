cb33cd5187e8fec6fb5f37b56d5bc7a8e1dc5614
deaundre <deaundrehicks@gmail.com>
2020-05-06 15:00:20 -0700

    added view and template for task details

--- tl_app/views.py
+++ tl_app/views.py
@@ -1,10 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Task
+
 
 # Create your views here.
 
 def task_list(request):
     tasks = Task.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
     return render(request, 'task_list.html', {'tasks':tasks})
+
+def task_detail(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    return render(request, 'task_detail.html', {'task': task})
 
